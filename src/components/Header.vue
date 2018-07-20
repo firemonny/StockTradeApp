29f32c9b4553944a6283a21c949c474c41ba43b2
firemonny <nschou@ucalgary.ca>
2018-07-20 16:57:51 -0600

    stock trade simple app

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -12,14 +12,14 @@
     </ul>
     <strong class="navbar-text navbar-right">Funds: {{funds}}</strong>
     <ul class="nav navbar-nav navbar-right">
-      <li><a href="#">End Day</a></li>
-      <li class="dropdown">
+      <li><a @click="endDay">End Day</a></li>
+      <li class="dropdown" :class="{open: isDropdownOPen}" @click="isDropdownOPen = !isDropdownOPen">
         <a href="#" 
         class="dropdown-toggle" 
         data-toggle="dropdown">Save & Load <span class="caret"></span></a>
         <ul class="dropdown-menu">
-          <li><a href="#">Save Data</a></li>
-          <li><a href="#">Load Data</a></li>
+          <li><a href="#" @click="saveData">Save Data</a></li>
+          <li><a href="#" @click="loadData">Load Data</a></li>
         </ul>
       </li>
     </ul>
@@ -27,10 +27,36 @@
 </nav>
 </template>
 <script>
+import { mapActions } from 'vuex';
 export default {
+  data(){
+    return{
+      isDropdownOPen:false
+    }
+  },
   computed:{
     funds(){
       return this.$store.getters.funds;
+    }
+  },
+  methods:{
+    ...mapActions({
+      randomizeStocks: 'randomizeStocks',
+      fetchData: 'loadData'
+    }),
+    endDay(){
+      this.randomizeStocks();
+          },
+    saveData(){
+      const data = {
+        funds: this.$store.getters.funds,
+        stockPortfolio: this.$store.getters.stockPortfolio,
+        stocks: this.$store.getters.stocks
+      };
+      this.$http.put('data.json',data);
+    },
+    loadData(){
+      this.fetchData();
     }
   }
 }
--- src/store/modules/stocks.js
+++ src/store/modules/stocks.js
@@ -8,14 +8,18 @@ const mutations = {
     'SET_STOCKS': (state, stocks) => {
         state.stocks= stocks;
     },
-    'RND_STOCKS': (state) => {}
+    'RND_STOCKS': (state) => {
+        state.stocks.forEach(stock => {
+            stock.price = Math.round(stock.price * (1 + Math.random()-0.05))
+        })
+    }
 }
 
 const actions = {
     buyStock: ({commit}, order) =>{
         commit('BUYSTOCK', order);
     },
-    initStocks: ({commit}, order)=>{
+    initStocks: ({commit}, stocks)=>{
         commit('SET_STOCKS',stocks);
     },
     randomizeStocks: ({commit}) =>{
